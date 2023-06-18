72fa7db6e246149e12938c355ee635e84c14ee89
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-06-18 12:44:43 +0630

    test

--- src/feeds.js
+++ src/feeds.js
@@ -9,7 +9,6 @@ function Feeds({feed,profile,reacter}){
     }
     return(
         <div className="lg:mb-72 2xl:mb-5 xl:mb-8 ">
-<<<<<<< HEAD
             {/* {   
                 
                 
@@ -31,44 +30,6 @@ function Feeds({feed,profile,reacter}){
             </div>
             <div className="">
                 <Feedcard obj={f} profile={profile}/>
-=======
-//             {   
-                
-                
-//                 feed.map((f)=>{
-//                     return <Feedcard obj={f} profile={profile} key={f.id} reacter={reacter}/>;
-//                 })
-//             }
-            <div className="">
-                <Feedcard obj={
-                    name:"kaung",
-                    status:"testing"
-                }profile={profile} />
-            </div>
-            <div className="">
-                <Feedcard obj={
-                    name:"kaung",
-                    status:"testing"
-                }profile={profile} />
-            </div>
-            <div className="">
-                <Feedcard obj={
-                    name:"kaung",
-                    status:"testing"
-                }profile={profile} />
-            </div>
-            <div className="">
-                <Feedcard obj={
-                    name:"kaung",
-                    status:"testing"
-                }profile={profile} />
-            </div>
-            <div className="">
-                <Feedcard obj={
-                    name:"kaung",
-                    status:"testing"
-                }profile={profile} />
->>>>>>> 269c43f8377ff5fa97c431e2ab2434b3551f13e5
             </div>
             {/* {
                 arr.map((arr)=>(
