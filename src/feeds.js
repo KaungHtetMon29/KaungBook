bff1fd43eacdb1c7976cf0586fe54385840a1b81
KaungHtetMon29 <99006407+KaungHtetMon29@users.noreply.github.com>
2023-06-18 12:22:30 +0630

    Update feeds.js

--- src/feeds.js
+++ src/feeds.js
@@ -5,28 +5,28 @@ import Feedcard from "./Feedcard";
 function Feeds({feed,profile,reacter}){
     return(
         <div className="lg:mb-72 2xl:mb-5 xl:mb-8 ">
-            {   
+//             {   
                 
                 
-                feed.map((f)=>{
-                    return <Feedcard obj={f} profile={profile} key={f.id} reacter={reacter}/>;
-                })
-            }
-            {/* <div className="">
-                <Feedcard name={uname} feed={feed} profile={profile} />
+//                 feed.map((f)=>{
+//                     return <Feedcard obj={f} profile={profile} key={f.id} reacter={reacter}/>;
+//                 })
+//             }
+            <div className="">
+                <Feedcard profile={profile} />
             </div>
             <div className="">
-                <Feedcard name={uname} feed={feed} profile={profile}/>
+                <Feedcard profile={profile}/>
             </div>
             <div className="">
-                <Feedcard name={uname} feed={feed} profile={profile}/>
+                <Feedcard profile={profile}/>
             </div>
             <div className="">
-                <Feedcard name={uname} feed={feed} profile={profile}/>
+                <Feedcard profile={profile}/>
             </div>
             <div className="">
-                <Feedcard name={uname} feed={feed} profile={profile}/>
-            </div> */}
+                <Feedcard profile={profile}/>
+            </div>
             {/* {
                 arr.map((arr)=>(
                     <Feedcard feed={arr}/>
