9d37a426778a06f34bd1ba818b8147eec804bde9
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-07-06 22:40:10 +0630

    feeds

--- src/pages/App.js
+++ src/pages/App.js
@@ -176,7 +176,7 @@ function App() {
     socket.on("latestpost", (data) => {
       const updatearray = [...friends, data[0]];
       setfriends(updatearray);
-      console.log(updatearray);
+      <Feeds feed={friends} reacter={user.name} />;
     });
   }, [socket]);
   return (
