d689c26c8f86d70a68b1a91bff6dbaeffeafbd5e
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-07-07 18:17:30 +0630

    registsign

--- src/pages/Regist.js
+++ src/pages/Regist.js
@@ -36,48 +36,48 @@ function Regist({ setregist, buff }) {
   };
 
   return (
-    <div className="shadow-md lg:m-auto lg:w-1/2 lg:my-96 2xl:m-auto 2xl:w-3/12 2xl:mt-20 xl:m-auto sm:m-auto sm:w-3/4 sm:mt-10 xl:w-4/12 xl:mt-10 lg:text-4xl sm:text-base xl:text-xl 2xl:text-lg bg-slate-200 rounded-3xl sm:rounded-lg flex flex-col justify-center">
-      <div className="lg:pb-5 lg:pt-10 xl:pt-3 xl:pb-5 xl:px-10 2xl:pt-3  2xl:pb-5 lg:px-20 2xl:px-10 sm:pb-1 sm:pt-5 sm:px-5">
-        <form className="flex flex-col lg:py-4 2xl:py-3 xl:py-1 sm:py-2 form">
-          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2">
+    <div className="shadow-md lg:m-auto lg:w-1/2 lg:my-96 2xl:m-auto 2xl:w-3/12 2xl:mt-20 xl:m-auto sm:m-auto sm:w-3/4 sm:mt-10 xs:m-auto xs:w-3/4 xs:mt-10 xl:w-4/12 xl:mt-10 lg:text-4xl sm:text-base xs:text-base xl:text-xl 2xl:text-lg bg-slate-200 rounded-3xl sm:rounded-lg xs:rounded-lg flex flex-col justify-center">
+      <div className="lg:pb-5 lg:pt-10 xl:pt-3 xl:pb-5 xl:px-10 2xl:pt-3  2xl:pb-5 lg:px-20 2xl:px-10 sm:pb-1 sm:pt-5 sm:px-5 xs:pb-1 xs:pt-5 xs:px-5">
+        <form className="flex flex-col lg:py-4 2xl:py-3 xl:py-1 sm:py-2 xs:py-2 form">
+          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2 xs:py-2 xs:px-2">
             Enter Email
           </label>
           <input
             onChange={onEmailchange}
             placeholder="Your Email address"
-            className="lg:rounded-2xl sm:rounded-xl border-2 lg:py-8 2xl:py-4 lg:px-6 2xl:px-4 sm:py-2 sm:px-2 xl:py-5"
+            className="lg:rounded-2xl sm:rounded-xl  xs:rounded-xl border-2 lg:py-8 2xl:py-4 lg:px-6 2xl:px-4 sm:py-2 sm:px-2 xs:py-2 xs:px-2 xl:py-5"
           />
         </form>
-        <form className="flex flex-col lg:py-4 2xl:py-3 sm:py-2 xl:py-1 form">
-          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2">
+        <form className="flex flex-col lg:py-4 2xl:py-3 sm:py-2 xs:py-2 xl:py-1 form">
+          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2 xs:py-2 xs:px-2">
             Enter Username
           </label>
           <input
             onChange={onUsernamechange}
             placeholder="Your Username "
-            className="lg:rounded-2xl sm:rounded-xl border-2 lg:py-8 2xl:py-4 lg:px-6 2xl:px-4 sm:py-2 sm:px-2 xl:py-5"
+            className="lg:rounded-2xl sm:rounded-xl xs:rounded-xl border-2 lg:py-8 2xl:py-4 lg:px-6 2xl:px-4 xs:py-2 xs:px-2 sm:py-2 sm:px-2 xl:py-5"
           />
         </form>
-        <form className="flex flex-col lg:py-4 2xl:py-3 sm:py-2 xl:py-1 form">
-          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2">
+        <form className="flex flex-col lg:py-4 2xl:py-3 sm:py-2 xs:py-2 xl:py-1 form">
+          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 xs:py-2 xs:px-2 sm:py-2 sm:px-2">
             Enter Password
           </label>
           <input
             onChange={onPwchange}
             placeholder="Your Password "
-            className="lg:rounded-2xl sm:rounded-xl border-2 lg:py-8 2xl:py-4 lg:px-6 2xl:px-4 sm:py-2 sm:px-2 xl:py-5"
+            className="lg:rounded-2xl sm:rounded-xl xs:rounded-xl border-2 lg:py-8 2xl:py-4 lg:px-6 2xl:px-4 xs:py-2 xs:px-2 sm:py-2 sm:px-2 xl:py-5"
           />
         </form>
       </div>
       <div className="flex justify-center pb-10 text-white">
         <button
-          className="lg:px-10 2xl:px-10 xl:px-10 xl:py-3 xl:rounded-lg 2xl:py-2 2xl:rounded-lg lg:py-5 bg-blue-400 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg"
+          className="lg:px-10 2xl:px-10 xl:px-10 xl:py-3 xl:rounded-lg 2xl:py-2 2xl:rounded-lg lg:py-5 bg-blue-400 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg xs:px-6 xs:py-2 xs:m-1 xs:rounded-lg"
           onClick={setregist}
         >
           Cancel
         </button>
         <button
-          className="lg:px-10 2xl:px-10 xl:px-10 xl:py-3 xl:rounded-lg 2xl:py-2 2xl:rounded-lg lg:py-5 bg-blue-400 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg"
+          className="lg:px-10 2xl:px-10 xl:px-10 xl:py-3 xl:rounded-lg 2xl:py-2 2xl:rounded-lg lg:py-5 bg-blue-400 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg xs:px-6 xs:py-2 xs:m-1 xs:rounded-lg"
           onClick={click}
         >
           Submit
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -73,50 +73,50 @@ function Signin({ setgetin, setregist, buff, friposts }) {
     // };
   }, []);
   return (
-    <div className="shadow-md lg:m-auto lg:w-1/2 2xl:m-auto 2xl:w-3/12 xl:w-4/12 lg:text-4xl 2xl:text-base xl:text-2xl xl:m-auto sm:m-auto sm:mt-10 sm:w-3/4 sm:text-base bg-slate-200 lg:rounded-3xl sm:rounded-xl lg:my-96 2xl:mt-10 xl:mt-10 sm:my-20 flex flex-col justify-center animate__animated animate__fadeIn ">
-      <div className="lg:pb-5 lg:pt-10 2xl:pb-0 xl:px-12 lg:px-20 2xl:px-10 sm:pb-1 sm:pt-5 sm:px-5 xl:pb-2 xl:pt-5">
-        <form className="flex flex-col lg:py-4 2xl:py-1 sm:py-2 xl:py-1 form">
-          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2 xl:px-2">
+    <div className="shadow-md lg:m-auto lg:w-1/2 2xl:m-auto 2xl:w-3/12 xl:w-4/12 lg:text-4xl 2xl:text-base xl:text-2xl xl:m-auto sm:m-auto xs:m-auto sm:mt-10 xs:mt-10 sm:w-3/4 xs:w-3/4 sm:text-base xs:text-base bg-slate-200 lg:rounded-3xl sm:rounded-xl xs:rounded-xl lg:my-96 2xl:mt-10 xl:mt-10 sm:my-20 xs:my-20 flex flex-col justify-center animate__animated animate__fadeIn ">
+      <div className="lg:pb-5 lg:pt-10 2xl:pb-0 xl:px-12 lg:px-20 2xl:px-10 sm:pb-1 sm:pt-5 sm:px-5 xs:pb-1 xs:pt-5 xs:px-5 xl:pb-2 xl:pt-5">
+        <form className="flex flex-col lg:py-4 2xl:py-1 sm:py-2  xs:py-2 xl:py-1 form">
+          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2 xs:py-2 xs:px-2 xl:px-2">
             Enter Email/Username
           </label>
           <input
             onChange={onEmailchange}
             placeholder="Your Email address or Username"
-            className="xl:px-4 lg:rounded-2xl sm:rounded-xl border-2 lg:py-8 xl:py-5 xl:text-base 2xl:py-4 lg:px-6 2xl:px-4 sm:py-2 sm:px-2"
+            className="xl:px-4 lg:rounded-2xl sm:rounded-xl xs:rounded-xl border-2 lg:py-8 xl:py-5 xl:text-base 2xl:py-4 lg:px-6 2xl:px-4 sm:py-2 sm:px-2 xs:py-2 xs:px-2"
           />
         </form>
-        <form className="flex flex-col lg:py-4 2xl:py-1 sm:py-2 xl:py-1 form">
-          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2 xl:px-2">
+        <form className="flex flex-col lg:py-4 2xl:py-1 sm:py-2 xs:py-2 xl:py-1 form">
+          <label className="grow lg:py-4 2xl:py-2 2xl:px-2 lg:px-4 sm:py-2 sm:px-2 xs:py-2 xs:px-2 xl:px-2">
             Enter Password
           </label>
           <input
             placeholder="Your Password "
             onChange={onPwchange}
-            className="lg:rounded-2xl sm:rounded-xl border-2 lg:py-8 xl:py-5 xl:text-base 2xl:py-4 lg:px-6 xl:px-4 2xl:px-4 sm:py-2 sm:px-2"
+            className="lg:rounded-2xl sm:rounded-xl xs:rounded-xl border-2 lg:py-8 xl:py-5 xl:text-base 2xl:py-4 lg:px-6 xl:px-4 2xl:px-4 sm:py-2 sm:px-2 xs:py-2 xs:px-2"
           />
         </form>
       </div>
       <div className="flex justify-center text-white xl:text-xl">
-        <button className="lg:px-10 2xl:px-8 xl:px-11 2xl:py-2 xl:py-4 2xl:rounded-lg lg:py-5 bg-blue-600 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg ">
+        <button className="lg:px-10 2xl:px-8 xl:px-11 2xl:py-2 xl:py-4 2xl:rounded-lg lg:py-5 bg-blue-600 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg xs:rounded-lg xs:px-6 xs:py-2 xs:m-1 ">
           Cancel
         </button>
         <button
-          className="lg:px-10 lg:py-5 2xl:px-8 xl:px-11 xl:py-4 2xl:py-2 2xl:rounded-lg bg-blue-600 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg "
+          className="lg:px-10 lg:py-5 2xl:px-8 xl:px-11 xl:py-4 2xl:py-2 2xl:rounded-lg bg-blue-600 lg:m-2 lg:rounded-2xl sm:px-6 sm:py-2 sm:m-1 sm:rounded-lg xs:px-6 xs:py-2 xs:m-1 xs:rounded-lg "
           onClick={click}
         >
           Submit
         </button>
       </div>
-      <div className="flex justify-center lg:text-3xl 2xl:text-lg sm:text-md xl:text-xl">
-        <button className="lg:px-12 2xl:px-9 lg:py-5 2xl:py-3 bg-white lg:m-2 lg:rounded-2xl  2xl:rounded-lg sm:px-5 sm:py-2 sm:my-1 lg:my-2 sm:rounded-lg flex items-center ">
+      <div className="flex justify-center lg:text-3xl 2xl:text-lg sm:text-md xs:text-md xl:text-xl">
+        <button className="lg:px-12 2xl:px-9 lg:py-5 2xl:py-3 bg-white lg:m-2 lg:rounded-2xl  2xl:rounded-lg sm:px-5 sm:py-2 sm:my-1 lg:my-2 sm:rounded-lg flex items-center  xs:px-6 xs:py-2 xs:my-1 xs:rounded-lg ">
           <img
             src={usericon}
-            className="lg:w-12 2xl:w-11 sm:w-6 lg:pr-4 sm:pr-2"
+            className="lg:w-12 2xl:w-11 sm:w-6 xs:w-6 lg:pr-4 sm:pr-2 xs:pr-2"
           />
           Signin with Google
         </button>
       </div>
-      <div className="flex justify-center lg:pb-16 sm:pb-8">
+      <div className="flex justify-center lg:pb-16 sm:pb-8 xs:pb-8">
         <button onClick={setregist}>Register</button>
       </div>
     </div>
